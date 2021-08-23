cc41f0b18704082ba4a43b33c025d98a9abed4b4
Evlampios Apostolidis <apostolid@iti.gr>
2021-08-23 21:02:59 +0300

    Create scorer.py

---
+++ model/layers/scorer.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import torch.nn as nn
+
+
+class Scorer(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers=2):
+        """Scoring LSTM"""
+        super().__init__()
+
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, bidirectional=True)
+        self.out = nn.Sequential(
+            nn.Linear(hidden_size * 2, 1),  # bidirection => scalar
+            nn.Sigmoid())
+
+    def forward(self, features, init_hidden=None):
+        """
+        Args:
+            features: [seq_len, 1, hidden_size]
+        Return:
+            scores [seq_len, 1]
+        """
+        self.lstm.flatten_parameters()
+
+        # [seq_len, 1, hidden_size * 2]
+        features, (h_n, c_n) = self.lstm(features)
+
+        # [seq_len, 1]
+        scores = self.out(features.squeeze(1))
+
+        return scores
+
+
+if __name__ == '__main__':
+
+    pass
