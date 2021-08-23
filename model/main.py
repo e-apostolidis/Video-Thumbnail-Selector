869d624d46652a4688dd7bbbdf6fe33ffd50bfa5
Evlampios Apostolidis <apostolid@iti.gr>
2021-08-23 21:02:53 +0300

    Create main.py

---
+++ model/main.py
@@ -0,0 +1,19 @@
+from configs import get_config
+from solver import Solver
+from data_loader import get_loader
+
+
+if __name__ == '__main__':
+    config = get_config(mode='train')
+    test_config = get_config(mode='test')
+
+    print(config.__dict__)
+    print(test_config.__dict__)
+
+    train_loader = get_loader(config.mode, config.video_type, config.split_index)
+    test_loader = get_loader(test_config.mode, test_config.video_type, test_config.split_index)
+    solver = Solver(config, train_loader, test_loader)
+
+    solver.build()
+    solver.evaluate(-1)  #evaluates the set of selected thumbnails using the randomly initialized network
+    solver.train()
