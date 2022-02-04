6a4c9223d412a8592f61a8a2ab7e91c111225acd
Evlampios Apostolidis <apostolid@iti.gr>
2022-02-04 15:36:19 +0200

    Update evaluate_exp.sh

--- evaluation/evaluate_exp.sh
+++ evaluation/evaluate_exp.sh
@@ -8,13 +8,13 @@ dataset_name=$3
 exp_path="$base_path/$exp_id/$dataset_name"
 
 echo "Extract training data from log files"
-for i in {0..9}; do
+for i in $(seq 0 1 9); do
 	path="$exp_path/logs/split$i"
-	python exportTensorFlowLog.py $path $path
+	python exportTensorFlowLog.py "$path" "$path"
 done
 
 echo "Evaluation using the top-3 selected user thumbnails"
-python compute_P@k.py $exp_path $dataset_name
+python compute_P@k.py "$exp_path" "$dataset_name"
 
 echo "Evaluation using the top-1 selected user thumbnail"
-python compute_P@k_on1thumb.py $exp_path $dataset_name
+python compute_P@k_on1thumb.py "$exp_path" "$dataset_name"
